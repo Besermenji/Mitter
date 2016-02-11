5fbdc3121da4dce64844fbd72aeff71a4aae388a
besermenji <besermenji.vladimir@gmail.com>
2016-02-11 14:28:32 +0100

    updated specs so they are not depricated, added faker so it generates random data in factory, passes all specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
+# class that represents mitter user
 class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :birth_date, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'has a valid factory' do
-    FactoryGirl.create(:user).should be_valid
+   expect(FactoryGirl.create(:user)).to be_valid
   end
 
   it 'is invalid without first name' do
-    FactoryGirl.build(:user, first_name: nil).should_not be_valid
+   expect(FactoryGirl.build(:user, first_name: nil)).not_to be_valid
   end
 
   it 'is invalid without last name' do
-    FactoryGirl.build(:user, last_name: nil).should_not be_valid
+   expect(FactoryGirl.build(:user, last_name: nil)).not_to be_valid
   end
 
   it 'is invalid without birth year' do
-    FactoryGirl.build(:user, birth_date: nil).should_not be_valid
+   expect(FactoryGirl.build(:user, birth_date: nil)).not_to be_valid
   end
 end
